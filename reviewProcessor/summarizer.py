73c8bda18048de70c1735cb9e47ee66789535a4c
scottlynn1 <scottlynn234@yahoo.com>
2025-03-14 12:25:37 -0400

    update summarizer to utilize llama 8b parameter llm

--- reviewProcessor/summarizer.py
+++ reviewProcessor/summarizer.py
@@ -1,15 +1,62 @@
-from transformers import pipeline
-import pythontopostgreslink
-import sys
+import os
+import nest_asyncio
+from llama_index.core.schema import TextNode
+from llama_index.core import get_response_synthesizer
+from llama_index.core import SummaryIndex
+from llama_index.llms.groq import Groq
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core import Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+import psycopg2
+from dotenv import load_dotenv
 
-# this program is a work in progress
-# would like to be able to summarize concatenated reviews but their is a token limit restriction
-# possible solution is to summarize several portions and the concatenate those and summarize recursively
+load_dotenv()
 
-text = pythontopostgreslink.get_reviews(sys.argv[1])
 
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-summary = summarizer(text, max_length=150, min_length=40, do_sample=False)
-print('summary')
-print(summary)
-print('summary')
+
+GROQ_API_KEY = os.environ.get('groq_key')
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+
+nest_asyncio.apply()
+
+llm = Groq(model="llama-3.1-8b-instant")
+
+Settings.llm = Groq(model="llama-3.1-8b-instant")
+Settings.embed_model = HuggingFaceEmbedding()
+
+conn = psycopg2.connect(database = "starbucksproject", 
+                          user = "scott", 
+                          host = 'localhost',
+                          password = os.environ.get('pg_password'),
+                          port = 5432
+                        )
+cur = conn.cursor()
+
+try:
+  cur.execute("SELECT review FROM reviews WHERE rating = 1 LIMIT 20;")
+  text = ''
+  for row in cur:
+    text = text + ' ' + row[0]
+      # cur.execute("INSERT INTO negative_words_before_2014 (words, occurrences) VALUES (%s, %s);", (words, count))
+    
+
+finally:
+  cur.close()
+  conn.commit()
+  conn.close()
+
+splitter = SentenceSplitter(chunk_size=2048)
+chunks = splitter.split_text(text)
+nodes = []
+for chunk in chunks:
+  nodes.append(TextNode(text=chunk))
+summary_index = SummaryIndex(nodes)
+
+summary_query_engine = summary_index.as_query_engine(
+    response_mode="tree_summarize",
+    use_async=True,
+)
+
+response = summary_query_engine.query("Summarize the given reviews of Starbucks")
+print(response)
